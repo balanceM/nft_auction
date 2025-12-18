7266f2c33ebf8591ea8e9b5571d309a817abca4d
liu <286515744@qq.com>
2025-12-18 14:30:28 +0800

    nftauction

--- contracts/NftAuction.sol
+++ contracts/NftAuction.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// chainlink
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract NftAuction {
+    address ethUsdAddr = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
     // 拍卖结构体
     struct Auction {
         uint256 startTime; // 起始时间
@@ -16,29 +20,77 @@ contract NftAuction {
 
         address nftContract; // NFT合约地址
         uint256 nftId; // NFT ID
+
+        address bidderTokenAddress; // 出价代币地址
     }
     // 拍卖列表
     Auction[] public auctions;
     // 管理员
     address public admin;
+    // 预言机价格表
+    mapping(address => AggregatorV3Interface) public priceFeeds;
+    // 管理员修饰符
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "You are not the admin");
+        _;
+    }
+    modifier onlyAuctionParties(uint256 _auctionId) {
+        Auction storage auction = auctions[_auctionId];
+        require(
+            msg.sender == admin ||
+            msg.sender == auction.seller,
+            "Not authorized"
+        );
+        _;
+    }
     // 初始化
     constructor() {
         admin = msg.sender;
     }
 
+    // 设置预言机价格表
+    function setPriceFeed(address tokenAddress, address _priceFeed) public onlyAdmin {
+        priceFeeds[tokenAddress] = AggregatorV3Interface(_priceFeed);
+    }
+
+    // 获取预言机价格
+    function getChainlinkDataFeedLatestAnswer(address tokenAddress) public view returns (int256) {
+        (
+        /* uint80 roundId */
+        ,
+        int256 answer,
+        /*uint256 startedAt*/
+        ,
+        uint256 updatedAt
+        ,
+        /*uint80 answeredInRound*/
+        ) = priceFeeds[tokenAddress].latestRoundData();
+        require(answer > 0, "Invalid price feed answer");
+        require(block.timestamp - updatedAt < 1 hours, "Price feed stale");
+        return answer;
+    }
+    
+    receive() external payable {}
+
+    fallback() external payable {}
+
     // 创建拍卖
     function createAuction(
-        uint256 _startTime, uint256 _duration, uint256 _startPrice, address _nftContract, uint256 _nftId
+        uint256 _duration, uint256 _startPrice, address _nftContract, uint256 _nftId
     ) external returns (uint256) {
         // 判断参数
-        require(_startTime >= block.timestamp, "Start time must be in the future");
-        require(_duration >= 3600, "Duration must be at least 1 hour");
+        require(_duration >= 10, "Duration must be at least 10s");
         require(_startPrice > 0, "Start price must be greater than 0");
-        require(address(_nftContract) != address(0), "NFT contract address must not be zero");
-        require(_nftId >= 0, "NFT ID must be greater or equal to 0");
+        require(_nftContract != address(0), "NFT contract address must not be zero");
+        // 授权卖家NFT给合约
+        // IERC721 nft = IERC721(_nftContract);
+        // require(nft.ownerOf(_nftId) == msg.sender, "Not NFT owner");
+        // nft.safeTransferFrom(msg.sender, address(this), _nftId);
+        IERC721(_nftContract).approve(address(this), _nftId);
         // 创建拍卖
+        uint256 auctionId = auctions.length;
         auctions.push(Auction({
-            startTime: _startTime,
+            startTime: block.timestamp,
             duration: _duration,
             startPrice: _startPrice,
             seller: msg.sender,
@@ -46,37 +98,60 @@ contract NftAuction {
             highestBid: 0,
             isEnd: false,
             nftContract: _nftContract,
-            nftId: _nftId
+            nftId: _nftId,
+            bidderTokenAddress: address(0)
         }));
-        return auctions.length - 1;
+        return auctionId;
     }
 
     // 出价
-    function bid(uint256 _auctionId, uint256 _amount) external {
+    function bid(uint256 _auctionId, uint256 _amount, address _tokenAddress) external payable {
         // 判断参数
         require(_auctionId < auctions.length, "Auction does not exist");
-        Auction memory auction = auctions[_auctionId];
-        require(_amount > auction.highestBid, "Amount must be greater than highest bid");
+        Auction storage auction = auctions[_auctionId];
         require(auction.isEnd == false, "Auction is ended");
         require(block.timestamp >= auction.startTime && block.timestamp <= auction.startTime + auction.duration, "Auction is not active");
+        // 统一价值尺度为USD
+        uint payValue;
+        if (_tokenAddress == address(0)) { // ETH 转 USD
+            uint ethPrice = uint(getChainlinkDataFeedLatestAnswer(ethUsdAddr));
+            payValue = msg.value * ethPrice;
+        } else { // ERC20代币 转 USD
+            payValue = _amount * uint(getChainlinkDataFeedLatestAnswer(_tokenAddress));
+        }
+        require(payValue > auction.highestBid, "Amount must be greater than highest bid");
+        // 转账给合约
+        IERC20(_tokenAddress).transferFrom(msg.sender, address(this), _amount);
         // 更新拍卖
-        auctions[_auctionId].highestBidder = msg.sender;
-        auctions[_auctionId].highestBid = _amount;
+        auction.highestBidder = msg.sender;
+        auction.highestBid = _amount;
+        auction.bidderTokenAddress = _tokenAddress;
+        // 退回之前的出价
+        if (auction.bidderTokenAddress == address(0)) { // 退回ETH
+            // (bool success, ) = payable(auction.highestBidder).call{value: auction.highestBid}("");
+            // require(success, "ETH transfer failed");
+            payable(auction.highestBidder).transfer(auction.highestBid);
+        } else { // 退回ERC20代币
+            IERC20(auction.bidderTokenAddress).transfer(auction.highestBidder, auction.highestBid);
+        }
     }
 
     // 结束拍卖
-    function endAuction(uint256 _auctionId) external {
+    function endAuction(uint256 _auctionId) external onlyAuctionParties(_auctionId) {
         // 判断参数
-        require(msg.sender == admin, "Only admin can end auction");
         require(_auctionId < auctions.length, "Auction does not exist");
-        Auction memory auction = auctions[_auctionId];
+        Auction storage auction = auctions[_auctionId];
         require(auction.isEnd == false, "Auction is ended");
         require(block.timestamp >= auction.startTime + auction.duration, "Auction is not ended");
         // 更新拍卖
-        auctions[_auctionId].isEnd = true;
-        // 转移NFT
-        IERC721(auction.nftContract).transferFrom(auction.seller, auction.highestBidder, auction.nftId);
-        // 转移ETH
-        payable(auction.seller).transfer(auction.highestBid);
+        auction.isEnd = true;
+        if (auction.highestBidder != address(0)) {
+            // 转移NFT
+            IERC721(auction.nftContract).transferFrom(auction.seller, auction.highestBidder, auction.nftId);
+            // 转移ETH
+            payable(auction.seller).transfer(auction.highestBid);
+        } else {
+            IERC721(auction.nftContract).transferFrom(address(this), auction.seller, auction.nftId);
+        }
     }
 }
